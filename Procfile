1497d4ce3604038ec7f12f17c5e89de94bc0f563
comhad <comhad@wearehackerone.com>
2021-05-20 21:48:21 +0100

    added comments

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wgsi:app
+web: gunicorn main:app
--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def home() :
 def discordWebhook(id, token) :
     msg = request.args.get('msg', default="No message was supplied") # If a user wants to include variables from post json like, name, they can just surround them by colons like :user:
 
-    if not request.json :
-        return jsonify(message="Not JSON", status = 400), 400
+    if not request.json : # If the request is not json refuse it, if we don't check this it will crash
+        return jsonify(message="Not JSON / header not application/json", status = 400), 400
         
     for each in request.json :
         replaceString = ":" + each + ":" # This is the variable string we want to replace
@@ -20,8 +20,7 @@ def discordWebhook(id, token) :
         msg = msg.replace(replaceString, replaceWith)
 
     webhookRepsonse = requests.post("https://discord.com/api/webhooks/" + id + "/" + token, data={ "content" : msg })
-    print(webhookRepsonse.text)
-    return jsonify(webhookStatus = webhookRepsonse.status_code, status = 200, message = "success")
+    return jsonify(webhookStatus = webhookRepsonse.status_code, status = 200, message = "success") # I don't know if to change this since its potentially misleading
 
 if __name__ == "__main__" :
     app.run()
